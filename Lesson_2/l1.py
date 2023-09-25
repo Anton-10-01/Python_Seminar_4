3a73abd085b548fa1326c8f347d0f3f0fb71c890
Anton <razjivin.anton@yandex.ru>
2023-09-25 20:08:40 +0300

    Работа со списками

---
+++ Lesson_2/l1.py
@@ -0,0 +1,10 @@
+list_1 = []
+list_2 = list()
+list_3 = [1, 2, 3, 8]
+# print(list_1)
+# print(*list_3) #Вывод без скобок и запятых
+
+# for i in list_3:
+#     print(i)
+
+# list_3.append(8) # Добавление в конец списка
